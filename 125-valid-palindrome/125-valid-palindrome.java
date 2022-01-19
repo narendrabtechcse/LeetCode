5c8b7cbd6fb79fab56ae0bbd6625d592a4e36771
Narendra Agrawal <narendrabtechcse@gmail.com>
2022-01-19 19:47:43 +0530

    Time: 37 ms (20.00%), Space: 45 MB (8.36%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-        s = s.toLowerCase().replaceAll("\\s", "");
-        
-       s = s.replaceAll("[^a-zA-Z0-9]", "");  
-        
+        s = s.toLowerCase().replaceAll("\\s", "").replaceAll("[^a-zA-Z0-9]", "");
         StringBuilder sb=new StringBuilder(s);  
-    sb.reverse();  
-    String rev=sb.toString();  
-    if(s.equals(rev)){  
-        return true;  
-    }else{  
-        return false;  
-    }  
-        
-        
+        sb.reverse();  
+        String rev=sb.toString();  
+        if(s.equals(rev)){  
+            return true;  
+        }else{  
+            return false;  
+        }  
     }
 }
