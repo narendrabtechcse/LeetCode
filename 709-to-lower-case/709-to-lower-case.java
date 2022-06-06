d6c006157d42e22e5b1a9533522be023a539accf
Narendra Agrawal <narendrabtechcse@gmail.com>
2022-06-06 19:54:52 +0530

    Time: 1 ms (64.40%), Space: 41.7 MB (66.38%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,16 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+      char[] lowerString=new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            if(Character.isUpperCase(s.charAt(i))){
+                int a=(int)s.charAt(i);
+                a=a+32;
+                lowerString[i]=(char)a;
+            }
+            else{
+                lowerString[i]=s.charAt(i);
+            }
+        }
+        return new String(lowerString);
     }
 }
