2abcae5ec88d021cee14ff3400d4fe7dc4adb0b7
Narendra Agrawal <narendrabtechcse@gmail.com>
2023-02-24 10:33:17 +0530

    Time: 17 ms (56.45%), Space: 46.1 MB (36.85%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,54 +1,65 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         
-        int n=str1.length();
-        int m=str2.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int []rows:dp){
-            Arrays.fill(rows,0);
-        }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+         int m = str1.length();
+        int n =  str2.length();
+
+        String lcs = all_longest_common_subsequences(str1,str2,m,n);
+        return lcs;
+    }
+
+    public static String all_longest_common_subsequences(String s, String t, int m, int n)
+    {
+        /* filling up the 2-d array with the maximum possible length of all subsequences */
+        int dp[][] = new int [m+1][n+1];
+
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+
+                if(s.charAt(i-1) == t.charAt(j-1)){
+
+                    dp[i][j] =  1 + dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i][j-1] , dp[i-1][j]);
                 }
             }
         }
-        String ans="";
-        int i=n,j=m;
-        while(i>0 && j>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                ans+=str1.charAt(i-1);
+
+      /** traversing back the array and adding up all the characters where the maximum subsequnce was possible with lcs */
+
+        StringBuilder str = new StringBuilder();
+
+        int i = m, j = n;
+
+          while(i>0 && j>0){
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                str.append(s.charAt(i-1));
                 i--;
                 j--;
-            }            
+            }
             else if(dp[i-1][j]>dp[i][j-1]){
-                ans+=str1.charAt(i-1);
+                str.append(s.charAt(i-1));
                 i--;
             }
-            else{
-                ans+=str2.charAt(j-1);
+            else if(dp[i-1][j]<=dp[i][j-1]){
+                str.append(t.charAt(j-1));
                 j--;
             }
+            
         }
+/* after getting the lcs String now we add all the leftover character from string 1 and string 2 into the lcs string */
+
         while(i>0){
-            ans+=str1.charAt(i-1);
+            str.append(s.charAt(i-1));
             i--;
         }
         while(j>0){
-            ans+=str2.charAt(j-1);
+            str.append(t.charAt(j-1));
             j--;
         }
-        String ss=new StringBuilder(ans).reverse().toString();
-        return ss;
+
+        return str.reverse().toString();
     }
+    
 }
