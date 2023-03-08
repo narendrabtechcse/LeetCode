6094adadc949890844b640364cd02e4d57116a16
Narendra Agrawal <narendrabtechcse@gmail.com>
2023-03-08 08:41:47 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (19.63%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,7 +1,17 @@
 class Solution {
     public int countVowelStrings(int n) {
         
-        return (n+4)*(n+3)*(n+2)*(n+1)/24;
+        //easy solution by maths return (n+4)*(n+3)*(n+2)*(n+1)/24;
+        
+        // dp solution 
+        
+        int[][] dp = new int[n+1][6];
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=5;j++) {
+                dp[i][j] = (i>1 ? dp[i-1][j] : 1) + dp[i][j-1];
+            }
+        }
+        return dp[n][5];
         
     }
 }
