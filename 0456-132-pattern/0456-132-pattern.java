475d1eef8786beac6b49103965d5f165e28a7908
Narendra Agrawal <narendrabtechcse@gmail.com>
2023-09-30 07:45:41 +0530

    Time: 26 ms (34.98%), Space: 63.2 MB (52.15%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,18 @@
+class Solution {
+   public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < secondMax) {
+                return true;
+            }
+            while (!st.isEmpty() && nums[i] > st.peek()) {
+                secondMax = Math.max(secondMax, st.pop());
+            }
+            st.push(nums[i]);
+        }
+
+        return false;
+    }
+}
