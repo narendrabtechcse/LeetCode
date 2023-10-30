3fb82cbdb2b90f0d9086ac1f58a32a020f604af4
Narendra Agrawal <narendrabtechcse@gmail.com>
2023-10-30 08:24:18 +0530

    Time: 2 ms (100.00%), Space: 43.7 MB (68.07%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,28 +1,44 @@
 class Solution {
-    public int[] sortByBits(int[] arr) {
-        Arrays.sort(arr);
-        List<Integer>[] countMap = new ArrayList[32];
-        for (int num : arr) {
-            int count = 0;
-            for (int i = 0; i < 32; i++) {
-                count += (num >> i) & 1;
-            } 
-            if (countMap[count] == null) {
-                countMap[count] = new ArrayList<>();
-            }
-            countMap[count].add(num);
-        }
-
-        int[] res = new int[arr.length];
-        int index = 0;
-        for (int i = 0; i < 32; i++) {
-            if (countMap[i] != null) {
-                for (int num : countMap[i]) {
-                    res[index] = num;
-                    index++;
-                }
-            }
-        }
-        return res;
+     public int[] sortByBits(int[] arr) {        
+        mergeSort(arr, 0, arr.length - 1);
+        return arr;
     }
+
+    private void mergeSort(int arr[], int i, int j) {
+        if (i == j) return;
+        int mid = (j - i) / 2 + i;
+        mergeSort(arr, i, mid);
+        mergeSort(arr, mid + 1, j);
+        mergeTwoSortedArray(arr, i, mid, j);
+    }
+
+    private void mergeTwoSortedArray(int arr[], int i, int mid, int j) {
+        
+        int sort[] =  new int[j-i+1];
+        int p1 = i, p2 = mid + 1;        
+        int itr = 0;
+        while (p1 <= mid && p2 <= j) {
+                            
+            if ( compare(arr[p1], arr[p2]) < 0) {                
+                sort[itr++] = arr[p1++];                
+            } else {
+                sort[itr++] = arr[p2++];                
+            }
+        }
+        while(p1<=mid) sort[itr++] = arr[p1++];     
+        while(p2<=j) sort[itr++] = arr[p2++];
+        System.arraycopy(sort,0,arr,i,sort.length);
+        
+    }
+
+
+    public int compare(int a, int b) {
+        int diff = Integer.bitCount(a) - Integer.bitCount(b);
+        // in case of two or more integers have the same number of 1's 
+        // you have to sort them in ascending order.
+        if(diff==0) return Integer.compare(a,b);
+        
+        else return diff;
+    }
+
 }
